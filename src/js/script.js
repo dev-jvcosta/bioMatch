aa7486458dd2d0b8dd07d5b56392494dd35e1170
dev-jvcosta <dev.jvcosta@gmail.com>
2025-05-20 21:42:14 -0300

    refactor: <add> Initial Object Cards

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+// Dados do Jogo: Pares das cartas (com imagem) e descrições com os cartões de curiosidade
+const cardPairs = [
+  {
+    image:{
+      title: "Cultura de Aspergillus flavius em Âgar",
+      src: "../../assets/images/img01.jpeg",
+    },
+    description: {
+      title: "Aspergillus que produz toxinas perigosas e cresce com cor verde-amarelada. Colônia após 7 dias de crescimento a 25 °C em ágar Czapek Yeast Extract",
+      fact: "Aspergillus flavus é conhecido por produzir aflatoxinas, potentes toxinas que podem contaminar alimentos como amendoim e milho, representando riscos significativos à saúde humana.",
+    },
+  },
+];
